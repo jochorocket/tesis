bb16e98178124cae73255f1a7378e181a45cb2c2
José Carlos Delgado Ramos <jocho@outlook.com>
2014-10-22 08:42:18 -0500

    Edicion posterior al parcial

---
+++ Docs/Puntos Cuanticos/example.m
@@ -0,0 +1,4 @@
+p = path;
+path (p,'/media/jocho/JOCHO/sanpa/seminario de tesis/Puntos Cuanticos/');
+num=xlsread('QDs-organizado_densidade.xls');
+%num=xlsread('PCAtest.xlsx');
--- ThesisDocs/Puntos Cuanticos/example.m
+++
@@ -1,4 +0,0 @@
-p = path;
-path (p,'/media/jocho/JOCHO/sanpa/seminario de tesis/Puntos Cuanticos/');
-num=xlsread('QDs-organizado_densidade.xls');
-%num=xlsread('PCAtest.xlsx');
